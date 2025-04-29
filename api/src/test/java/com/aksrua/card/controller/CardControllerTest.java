0a25d6025346493ddfc085918435745121e1d5d4
김용환 <clazziquai01@gmail.com>
2025-04-29 21:37:36 +0900

    [test] CardControllerTest 작성

--- api/src/test/java/com/aksrua/card/controller/CardControllerTest.java
+++ api/src/test/java/com/aksrua/card/controller/CardControllerTest.java
@@ -1,17 +1,55 @@
 package com.aksrua.card.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
 
+import com.aksrua.card.dto.response.CardResponseDto;
+import com.aksrua.card.service.CardService;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@AutoConfigureMockMvc
-@WebMvcTest(CardController.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 class CardControllerTest {
 
+	private MockMvc mockMvc;
+	private CardService cardService;
+
+	@BeforeEach
+	void setUp() {
+		cardService = Mockito.mock(CardService.class);
+		CardController cardController = new CardController(cardService);
+		mockMvc = MockMvcBuilders.standaloneSetup(cardController).build();
+	}
+
 	@Test
-	public void test_test() {
-		assertEquals(1, 1);
+	@DisplayName("10장의 소개팅 카드를 받는다")
+	public void getCardList() throws Exception {
+		List<CardResponseDto> mockCardList = IntStream.range(0, 10)
+				.mapToObj(i -> CardResponseDto.builder()
+						.id((long) i)
+						.nickname("user" + i)
+						.age(22 + i)
+						.gender("F")
+						.distanceKm(20 + i)
+						.imagesUrl(null)
+						.hobbies(null)
+						.build())
+				.toList();
+
+		when(cardService.getCardList()).thenReturn(mockCardList);
+
+		mockMvc.perform(get("/api/v1/cards").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()", is(10)));
 	}
 }

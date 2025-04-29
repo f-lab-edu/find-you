fa300e47ef2bbacd243c6e08c9114d979274a6aa
김용환 <clazziquai01@gmail.com>
2025-04-29 21:41:11 +0900

    [refactor] Car 조회 로직 수정

--- api/src/main/java/com/aksrua/card/repository/impl/InMemoryCardRepository.java
+++ api/src/main/java/com/aksrua/card/repository/impl/InMemoryCardRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryCardRepository implements CardRepository {
-	private final Map<Long, Card> store = new HashMap<>();
+	private Map<Long, Card> store = new HashMap<>();
 	private long seq = 1L;
 
 	InMemoryCardRepository() {

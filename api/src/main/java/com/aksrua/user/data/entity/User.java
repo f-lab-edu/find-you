d4bec220ccebe3b5ee98c8021251597775bae5bf
kim Yong Hwan <74572506+GyeomFka@users.noreply.github.com>
2025-05-11 16:16:20 +0900

    Feature/user signup (#6)
    * [docs] database의존성 추가
    
    * [docs] jpa 환경설정
    
    * [docs] docker, mysql 환경설정
    
    * [feat] inmemory 형식 -> jpa repository 변경
    
    * [feat] inmemory 형식 -> jpa repository 변경
    
    * [docs] docker 파일 변경
    - Dockerfile 제거
    - docker-compose : spring-app 속성 제거
    
    * [feat] User 회원가입 기능
    
    * [feat] User 가입 후 해당 User의 Card생성 기능
    
    * [docs] application.yml dir변경

---
+++ api/src/main/java/com/aksrua/user/data/entity/User.java
@@ -0,0 +1,72 @@
+package com.aksrua.user.data.entity;
+
+import com.aksrua.card.data.entity.Card;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String username;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String phoneNumber;
+
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Card card;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				", email='" + email + '\'' +
+				", phoneNumber='" + phoneNumber + '\'' +
+				", card=" + card +
+				", createdAt=" + createdAt +
+				", updatedAt=" + updatedAt +
+				'}';
+	}
+}

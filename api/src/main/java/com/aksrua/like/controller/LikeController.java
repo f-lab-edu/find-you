4812d4746f30a691d4c0b03be1e942fe547b8fa6
kim Yong Hwan <74572506+GyeomFka@users.noreply.github.com>
2025-05-18 17:12:19 +0900

    0518 (#14)
    * [feat] 좋아요 목록 카드 조회
    
    * [feat] 좋아요 목록 카드 조회 테스트
    
    * [feat] 좋아요 보내기 및 좋아요 취소
    
    * [docs] update readme

---
+++ api/src/main/java/com/aksrua/like/controller/LikeController.java
@@ -0,0 +1,50 @@
+package com.aksrua.like.controller;
+
+import com.aksrua.card.data.entity.Card;
+import com.aksrua.card.data.repository.CardRepository;
+import com.aksrua.like.data.entity.Like;
+import com.aksrua.like.dto.request.SendLikeRequestDto;
+import com.aksrua.like.dto.response.SendLikeResponseDto;
+import com.aksrua.like.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class LikeController {
+
+	private final LikeService likeService;
+	private final CardRepository cardRepository;
+
+	@PostMapping("/likes")
+	public ResponseEntity<SendLikeResponseDto> sendLike(@RequestBody SendLikeRequestDto requestDto) {
+		Card senderCard = cardRepository.findByUserId(requestDto.getSenderCardId());
+		Card receiverCard = cardRepository.findByUserId(requestDto.getReceiverCardId());
+
+		Like like = Like.builder()
+				.senderCard(senderCard)
+				.receiverCard(receiverCard)
+				.build();
+
+		Like sendedLike = likeService.likeSend(like);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(SendLikeResponseDto.fromEntity(sendedLike));
+	}
+
+	@DeleteMapping("/likes/{likeId}")
+	public ResponseEntity<Void> removeLike(@PathVariable final Long likeId) {
+		likeService.removeLike(likeId);
+		return ResponseEntity.noContent().build();
+	}
+}
+

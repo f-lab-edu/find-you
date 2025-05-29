e1883f3fe455ceeb35a81bd0faf0ebe16f08a5ff
김용환 <clazziquai01@gmail.com>
2025-05-30 02:44:20 +0900

    [feat] - Card Entity gender 제거 - Enum 타입 추가

---
+++ api/src/main/java/com/aksrua/card/data/entity/BodyType.java
@@ -0,0 +1,20 @@
+package com.aksrua.card.data.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum BodyType {
+	SLIM("슬림"),
+	SLIM_TONED("슬림탄탄"),
+	NORMAL("보통"),
+	MUSCULAR("근육질"),
+	CHUBBY("약간통통"),
+	FAT("통통"),
+	ANY("상관없음");
+
+	private final String description;
+
+	BodyType(String description) {
+		this.description = description;
+	}
+}
---
+++ api/src/main/java/com/aksrua/card/data/entity/Religion.java
@@ -0,0 +1,18 @@
+package com.aksrua.card.data.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Religion {
+	NONE("없음"),
+	CHRISTIANITY("기독교"),
+	CATHOLICISM("천주교"),
+	BUDDHISM("불교"),
+	ANY("상관없음");
+
+	private final String description;
+
+	Religion(String description) {
+		this.description = description;
+	}
+}

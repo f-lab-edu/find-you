4a5f25afd606ef6ff59856c3dc96b06405bec7a2
김용환 <clazziquai01@gmail.com>
2025-06-03 23:20:34 +0900

    [feat] 응답 형태 통일(예외 포함)

--- api/src/main/java/com/aksrua/common/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/aksrua/common/exception/GlobalExceptionHandler.java
@@ -1,15 +1,37 @@
 package com.aksrua.common.exception;
 
+import com.aksrua.common.dto.ApiResponse;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ResponseStatus(HttpStatus.CONFLICT)
 	@ExceptionHandler(DuplicateResourceException.class)
-	public ResponseEntity<String> handleUserAlreadyExists(DuplicateResourceException e) {
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+	public ApiResponse<String> handleUserAlreadyExists(DuplicateResourceException e) {
+		return ApiResponse.of(
+				HttpStatus.CONFLICT,
+				e.getMessage(),
+				null
+		);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BindException.class)
+	public ApiResponse<Object> bindException(BindException e) {
+		return ApiResponse.of(
+				HttpStatus.BAD_REQUEST,
+				e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+				null
+		);
 	}
 }

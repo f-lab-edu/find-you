490efffe39e0ee0b202de07c510b63abd8a8e865
김용환 <clazziquai01@gmail.com>
2025-06-13 02:36:16 +0900

    [feat] 인증 인가 구현_예외처리 #26

--- api/src/main/java/com/aksrua/card/controller/CardController.java
+++ api/src/main/java/com/aksrua/card/controller/CardController.java
@@ -2,6 +2,7 @@ package com.aksrua.card.controller;
 
 import static com.aksrua.filter.dto.response.FilterResponseDto.fromEntity;
 
+import com.aksrua.auth.service.AuthTokenService;
 import com.aksrua.card.controller.dto.response.CardListResponseDto;
 import com.aksrua.card.data.entity.Card;
 import com.aksrua.card.controller.dto.request.CreateCardRequestDto;
@@ -17,14 +18,16 @@ import com.aksrua.filter.dto.response.FilterResponseDto;
 import com.aksrua.filter.dto.response.UpdateFilterResponseDto;
 import com.aksrua.filter.service.FilterService;
 import com.aksrua.interaction.data.entity.Like;
-import com.aksrua.interaction.data.entity.LikeStatus;
 import com.aksrua.interaction.dto.response.RemoveLikeResponseDto;
 import com.aksrua.interaction.dto.response.SendLikeResponseDto;
+import com.aksrua.interaction.dto.response.SentLikeCardsListResponseDto;
 import com.aksrua.interaction.service.InteractionService;
 import com.aksrua.user.data.entity.User;
 import com.aksrua.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -34,6 +37,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,11 +51,12 @@ public class CardController {
 	private final UserService userService;
 	private final FilterService filterService;
 	private final InteractionService interactionService;
+	private final AuthTokenService authTokenService;
 
 	@PostMapping("/cards")
 	public ApiResponse<CreateCardResponseDto> createCard(@Valid @RequestBody CreateCardRequestDto requestDto) {//TODO: 인증방식 도입
-		User joinedUser = userService.getUserDetail(requestDto.getUserId());
-		Card card = requestDto.toEntity(joinedUser);
+		User joinedUser = userService.getUserDetails(requestDto.getUserId());
+		Card card = requestDto.toEntity(joinedUser); //TODO: 여기 어디선가 birth_date를 age로 변환하는데 그거 card entity에서 해주기
 		Card createdCard = cardService.createCard(card);
 		return ApiResponse.of(HttpStatus.CREATED, CreateCardResponseDto.fromEntity(createdCard));
 	}
@@ -65,11 +70,13 @@ public class CardController {
 	}
 
 	/**
-	 * @desc: 조회도 서비스적으로 비용이 필요한 부분이다. 아무나 조회를 못 하도록 해야하는데...
+	 * TODO: 상대방 조회도 비즈니스적으로 필요한 부분이다. 아무나 조회를 못 하도록 해야하는데...
 	 */
 	@GetMapping("/cards/{userId}")
-	public ApiResponse<CardResponseDto> getCardDetail(@PathVariable Long userId) {
-		return ApiResponse.ok(CardResponseDto.fromEntity(cardService.getCardDetails(userId)));
+	public ApiResponse<CardResponseDto> getCardDetail(@PathVariable Long userId, HttpServletRequest request) {
+													  /*@RequestHeader("X-Auth-Token") String authToken,*/
+		Long authId = (Long) request.getAttribute("userId");
+		return ApiResponse.ok(CardResponseDto.fromEntity(cardService.getCardDetails(userId, authId)));
 	}
 
 	@GetMapping("/cards/{userId}/filter")
@@ -82,7 +89,7 @@ public class CardController {
 	public ApiResponse<CreateFilterResponseDto> createFilter(@PathVariable Long userId, @RequestBody CreateFilterRequestDto requestDto) {
 		//TODO: user 조회를 성능적으로 보완한 jpa getReferenceById 권장사항
 //		User findUser = userService.getReferenceById(userId);
-		User findUser = userService.getUserDetail(userId);
+		User findUser = userService.getUserDetails(userId);
 		Filter filter = requestDto.toEntity(findUser);
 		Filter savedFilter = filterService.createFilter(filter);
 		return ApiResponse.of(HttpStatus.CREATED, CreateFilterResponseDto.fromEntity(savedFilter));
@@ -91,7 +98,7 @@ public class CardController {
 	@PatchMapping("/cards/{userId}/filter")
 	public ApiResponse<UpdateFilterResponseDto> updateFilter(@PathVariable Long userId, @RequestBody UpdateFilterRequestDto requestDto) {
 //		User findUser = userService.getReferenceById(userId);
-		User findUser = userService.getUserDetail(userId);
+		User findUser = userService.getUserDetails(userId);
 		Filter filter = requestDto.toEntity(findUser);
 		Filter updateFilter = filterService.updateFilter(userId, filter);
 		return ApiResponse.ok(UpdateFilterResponseDto.fromEntity(updateFilter));
@@ -105,15 +112,38 @@ public class CardController {
 		return ApiResponse.of(HttpStatus.CREATED, SendLikeResponseDto.fromEntity(sentLike));
 	}
 
+	@GetMapping("/cards/{userId}/likes/sent")
+	public ApiResponse<List<SentLikeCardsListResponseDto>> getSentLikesList(@PathVariable Long userId) {
+		List<SentLikeCardsListResponseDto> sentLikeCardsList = interactionService.getSentLikesList(userId)
+				.stream()
+				.map(SentLikeCardsListResponseDto::fromEntity)
+				.collect(Collectors.toList());
+		return ApiResponse.ok(sentLikeCardsList);
+	}
+
 	@DeleteMapping("/cards/{userId}/likes/{likeId}")
-	public ApiResponse<?> removeLike(@PathVariable Long userId, @PathVariable Long likeId) {
+	public ApiResponse<RemoveLikeResponseDto> removeLike(@PathVariable Long userId, @PathVariable Long likeId) {
 		interactionService.removeLike(userId, likeId);
 		return ApiResponse.of(HttpStatus.NO_CONTENT, new RemoveLikeResponseDto(likeId, "삭제가 완료 되었습니다."));
+	}
+
+	@GetMapping("/cards/{userId}/likes/received")
+	public void getReceivedLikesList(@PathVariable Long userId) {
+
+	}
+
+	@GetMapping("/cards/{userId}/likes/sent/{likeId}")
+	public void getSentLikeDetails(@PathVariable Long userId, @PathVariable Long likeId) {
+
+	}
+
+	@GetMapping("/cards/{userId}/likes/received/{likeId}")
+	public void getReceivedLikeDetails(@PathVariable Long userId, @PathVariable Long likeId) {
+		//TODO: 받은 좋아요를 보면은 해당 LIKE의 상태를 변화 시켜줘야 한다.
 	}
 
 	@PostMapping("/cards/{userId}/dislikes/{cardId}")
 	public void sendDislike(@PathVariable Long userId, @PathVariable Long cardId) {
 
 	}
-
 }
--- api/src/main/java/com/aksrua/interaction/service/InteractionService.java
+++ api/src/main/java/com/aksrua/interaction/service/InteractionService.java
@@ -1,11 +1,14 @@
 package com.aksrua.interaction.service;
 
-import com.aksrua.interaction.data.entity.Dislike;
+import com.aksrua.card.data.entity.Card;
+import com.aksrua.card.service.CardService;
 import com.aksrua.interaction.data.entity.Like;
-import com.aksrua.interaction.data.entity.LikeStatus;
 import com.aksrua.interaction.data.repository.DislikeRepository;
 import com.aksrua.interaction.data.repository.LikeRepository;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,10 +21,7 @@ public class InteractionService {
 	private final LikeValidator likeValidator;
 	private final LikeRepository likeRepository;
 	private final DislikeRepository dislikeRepository;
-
-	/**
-	 * 조회 기능
-	 */
+	private final CardService cardService;
 
 	@Transactional
 	public Like sendLike(Like like) {
@@ -49,12 +49,29 @@ public class InteractionService {
 	@Transactional
 	public void removeLike(Long userId, Long likeId) {
 		Like like = likeFinder.findLikeById(likeId);
-
 		//TODO: 인증 인가 검증
 		likeValidator.validateRemovalPermission(like, userId);
-
 		likeRepository.deleteByIdAndSenderCardId(likeId, userId);
 	}
+
+	/**
+	 * 내가 보낸 좋아요 리스트 조회 기능
+	 */
+	public List<Card> getSentLikesList(Long userId) {
+		List<Long> receiverCardIdList = likeRepository.findReceiverCardIdsBySenderCardId(userId);
+		return cardService.getCardsListByIdIn(receiverCardIdList);
+	}
+
+	/**
+	 * 내가 받은 좋아요 리스트 조회 기능
+	 */
+	public void getReceivedLikesList() {
+
+	}
+
+	/**
+	 * 좋아요 상세 조회 기능
+	 */
 
 	/**
 	 *  싫어요 보내기
